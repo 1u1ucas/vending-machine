4dd1b8a6eb307b83ea0587cffdfd8521500d77c4
1u1ucas <lucasleveque17@gmail.com>
2024-10-07 10:24:32 +0200

    --

---
+++ vending-machine.ts
@@ -0,0 +1,45 @@
+class VendingMachine {
+
+  private isOn: boolean = false;
+
+  private snacksQty: number = 50;
+
+  private money: number = 0;
+
+  public buySnack() {
+
+    if (!this.isOn) {
+      throw new Error('Vending machine is off');
+    }
+
+    if (this.snacksQty === 0) {
+      throw new Error('No snacks left');
+    }
+
+
+    this.snacksQty--;
+    this.money += 2;
+
+    return 'Enjoy your snack!';
+
+  }
+
+  public reset() {
+    this.isOn = false;
+    this.snacksQty = 50;
+    this.money = 0;
+  }
+
+  public turnOn() {
+    this.isOn = true;
+  }
+
+  public shootWithFoot() {
+
+    if(this.snacksQty < 5) {}
+    this.snacksQty-= 5;
+    this.money += 20;
+    this.isOn = false;
+  }
+
+}
